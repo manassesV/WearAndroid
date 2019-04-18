e1c384c7066a3da0ea78f410ad83cb57a0940dee
ManassesV <manavitorino@gmail.com>
2019-04-17 21:46:25 -0300

    Initial commit

---
+++ mobile/src/main/java/com/manasses/manab/myapplication/DataLayerListenerService.kt
@@ -0,0 +1,19 @@
+package com.manasses.manab.myapplication
+
+import android.util.Log
+import com.google.android.gms.wearable.MessageEvent
+import com.google.android.gms.wearable.WearableListenerService
+
+class DataLayerListenerService: WearableListenerService(){
+
+
+    override fun onMessageReceived(p0: MessageEvent?) {
+        super.onMessageReceived(p0)
+
+        when(p0?.path){
+            "/PROXIMO" -> {
+                Log.i("WEAR", "BOTAO CLICADO")
+            }
+        }
+    }
+}
